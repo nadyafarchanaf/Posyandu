ff3070bce72363ab109afc87183c202345d695cc
hafidh19 <hafidh_19@apps.ipb.ac.id>
2019-05-01 19:31:19 +0700

    Login

---
+++ app/src/main/java/com/example/nadya/posyandu/Util/ServerAPI.java
@@ -0,0 +1,6 @@
+package com.example.nadya.posyandu.Util;
+
+public class ServerAPI {
+    public static String URL_REGISTER = "http://192.168.1.13/Posyandu/register.php";
+    public static String URL_LOGIN = "http://192.168.1.13/Posyandu/login.php";
+}
