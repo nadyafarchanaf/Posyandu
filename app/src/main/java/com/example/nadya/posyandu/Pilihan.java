4aa0584113db6f9ce9180c00ea07410d39e82172
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2019-05-11 12:58:29 +0700

    splashscreen baru nih

---
+++ app/src/main/java/com/example/nadya/posyandu/Pilihan.java
@@ -0,0 +1,13 @@
+package com.example.nadya.posyandu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pilihan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilihan);
+    }
+}
