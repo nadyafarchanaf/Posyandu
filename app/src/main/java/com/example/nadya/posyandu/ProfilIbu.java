f4da11b40e87b8f1e15716af1c533c90beda8744
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2019-05-25 06:46:41 +0700

    form pemeriksaan

---
+++ app/src/main/java/com/example/nadya/posyandu/ProfilIbu.java
@@ -0,0 +1,13 @@
+package com.example.nadya.posyandu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilIbu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_ibu);
+    }
+}
--- app/src/main/java/com/example/nadya/posyandu/Util/ServerAPI.java
+++ app/src/main/java/com/example/nadya/posyandu/Util/ServerAPI.java
@@ -1,6 +1,6 @@
 package com.example.nadya.posyandu.Util;
 
 public class ServerAPI {
-    public static String URL_REGISTER = "http://192.168.1.13/Posyandu/register.php";
-    public static String URL_LOGIN = "http://192.168.1.13/Posyandu/login.php";
+    public static String URL_REGISTER = "http://192.168.1.13/PemeriksaanMedis/register.php";
+    public static String URL_LOGIN = "http://192.168.1.13/PemeriksaanMedis/login.php";
 }
